feaab99e471e5064fce0d3f7e97f8d2ff365ae84
harisha Annapareddy <harishaannapareddy@my.unt.edu>
2017-09-07 20:54:58 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -10,6 +10,15 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
+  if(13<=age<=19){
+    return "You are a teenager!"
+  else if (age<=12) {return "Youare a kid"}
+  else {return "You are not a teenager"
+    
+  }
+    
+  }
+  }
 
 }
 
