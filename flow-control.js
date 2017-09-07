d58ab6f9c82ce541eac2129cc4c5bf14ceccb960
harisha Annapareddy <harishaannapareddy@my.unt.edu>
2017-09-07 22:26:33 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -26,7 +26,7 @@ function ageChecker(age) {
 function ternaryTeenager(age) {
   if(age>=13 && age<=19){
     return "You are a teenager!";}
-    else{ return "You are not a tennager";}
+  else{ return "You are not a tennager";}
 }
 
 function switchAge(age) {
